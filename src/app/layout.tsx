38f5c3890bf6de3e299b9164e3647f24e1d151a1
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2025-07-31 12:26:40 +0530

    feat: initialize next.js project with tailwind and shadcn/ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Onest } from "next/font/google";
+import "./globals.css";
+
+const onest = Onest({
+  variable: "--font-onest",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Valour",
+  description: "Valour by Boat",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${onest.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
